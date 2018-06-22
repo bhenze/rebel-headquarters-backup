ab42fb6d7c68e883a396e4baa52fa788556786fe
Brian Henze <brian.henze@hashrebel.com>
2018-06-22 12:13:37 -0700

    fixing package manager

--- static/sw.js
+++ static/sw.js
@@ -1,35 +1,35 @@
 importScripts('/_nuxt/workbox.3de3418b.js')
 
 const workboxSW = new self.WorkboxSW({
-  "cacheId": "order-pwa-poc",
+  "cacheId": "rebel-headquarters-sandbox-test",
   "clientsClaim": true,
   "directoryIndex": "/"
 })
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/app.41a0e116f7a119c0d828.js",
-    "revision": "6cc564128669c94d037bd0315cbcff20"
+    "url": "/_nuxt/app.9a84b727d4416c5a53c6.js",
+    "revision": "e5b0620d02598f50e718c1bbde4ecaba"
   },
   {
-    "url": "/_nuxt/layouts/default.6de1868c603fcd6679d6.js",
-    "revision": "c6433ebf49eb782fdda87206a82cfb4f"
+    "url": "/_nuxt/layouts/default.12f2c1884deaee890645.js",
+    "revision": "bffe7fad8e41fba188a92e1f7a52a03c"
   },
   {
-    "url": "/_nuxt/manifest.120393d3e37b7cf91096.js",
-    "revision": "fd40d729a3f31772ee6f372b9a3bdcae"
+    "url": "/_nuxt/manifest.2dc1fbff803c5178573d.js",
+    "revision": "56012a433dfd0b437616ddf335fe786d"
   },
   {
-    "url": "/_nuxt/pages/catolog.d3dc9f852c8ce9f0c545.js",
-    "revision": "c2cb982f6108a709eba37fcef23de137"
+    "url": "/_nuxt/pages/index.55f13b2e10a067e3143b.js",
+    "revision": "3a071a2f3bc8913e0a45dfd0729bca35"
   },
   {
-    "url": "/_nuxt/pages/index.117bafdc737a0b64748e.js",
-    "revision": "c65e751326d6970e28416871c13eff8c"
+    "url": "/_nuxt/pages/resume.e03c299072016d633563.js",
+    "revision": "b0222062cda55120c9432c62a8fa2a53"
   },
   {
-    "url": "/_nuxt/vendor.14643f6166d3a87f9ab2.js",
-    "revision": "0ac2c27b1bd64b1a9ff86f4c35eddac7"
+    "url": "/_nuxt/vendor.9af420cceaa6eb552a5e.js",
+    "revision": "1c52e27ddcec242663fc357e0b735d54"
   }
 ])
 
